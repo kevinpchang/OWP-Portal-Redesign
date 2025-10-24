88fad633135880a7032c06951384c4ed76698dbc
Nick Guzman <nickguzmandgt1@gmail.com>
2025-10-24 00:17:34 -0700

    Updated Courses button, added base Courses page.

--- frontend/src/page_components/SidebarComponent.vue
+++ frontend/src/page_components/SidebarComponent.vue
@@ -16,9 +16,20 @@
       <div class="slides-button" :class="{ active: route.name === 'MyAccountPage'}">
         <div class="button-text">Slides</div>
       </div>
-      <div class="courses-button" :class="{ active: route.name === 'MyAccountPage' }">
-        <div class="button-text">Courses</div>
+
+      <!--CoursesButton Start-->
+      <div class="courses-button" :class="{ active: route.name === 'CoursesPage'}">
+        <router-link to="/courses" class="courses-link">
+          <div class="button-icon">
+            <div class="line"><span class="dot"></span><span class="bar"></span></div>
+            <div class="line"><span class="dot"></span><span class="bar"></span></div>
+            <div class="line"><span class="dot"></span><span class="bar"></span></div>
+          </div>
+          <div class="button-text">Courses</div>
+        </router-link> 
       </div>
+      <!--CoursesButton End-->
+
       <div class="operator-numbers-button" :class="{ active: route.name === 'MyAccountPage' }">
         <div class="button-text">Operator Numbers</div>
       </div>
@@ -96,4 +107,61 @@
   .sidebar .active {
     background-color: #F2F1F2;
   }
+
+/*CoursesButton Start*/
+.courses-button {
+    display: flex;
+    align-items: center;
+    width: 250px;
+    height: 47px;
+    border-radius: 8px;
+    margin-top: 21px;
+  }
+  .courses-button.active {
+    background-color: #F2F1F2;
+  }
+  .courses-button .courses-link {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    text-decoration: none;
+    color: inherit;
+    width: 100%;
+    padding-left: 39.25rem;
+  }
+  .courses-button .button-text {
+    font-size: 20px;
+    color: #034750;
+    font-family: "Myriad Pro Semibold";
+    font-weight:600;
+    margin: 0;
+  }
+  .courses-button .button-icon {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 20px;
+    height: 16px;
+    position: absolute;
+    left: calc(39.25rem - 8px);
+  }
+  .courses-button .button-icon .line {
+    display: flex;
+    align-items: center;
+    height: 4px;
+    gap: 4px;
+  }
+  .courses-button .button-icon .dot {
+    width: 4px;
+    height: 4px;
+    border-radius: 50%;
+    background-color: #034750;
+  }
+  .courses-button .button-icon .bar {
+    flex: 1;
+    height: 2px;
+    background-color: #034750;
+  }
+  /*CoursesButton end*/
+
 </style>
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import DashboardPage from '../pages/DashboardPage.vue'
 import MyAccountPage from '../pages/MyAccountPage.vue'
+import CoursesPage from '../pages/CoursesPage.vue'
 
 const routes = [
   //{path: "/", name: 'DashboardPage', component: DashboardPage} //KEVIN'S DASHBOARD
-  {path: "/", name: 'MyAccountPage', component: MyAccountPage} // CRISTOBE
+  {path: "/", name: 'MyAccountPage', component: MyAccountPage}, // CRISTOBE
   // L'S MY_ACCOUNT
+  { path: '/courses', name: 'CoursesPage', component: CoursesPage,}
 ]
 
 const router = createRouter({
