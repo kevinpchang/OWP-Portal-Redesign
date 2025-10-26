07c66f15c042e2c7549beac426c624223042010c
Uday K <107959366+Udaykyama@users.noreply.github.com>
2025-10-26 11:07:03 -0700

    Add CertificatesPage and configure route
    Adds the CertificatePage component to the router and configures a route for certificates.

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,12 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 import DashboardPage from '../pages/DashboardPage.vue'
 import MyAccountPage from '../pages/MyAccountPage.vue'
 import CoursesPage from '../pages/CoursesPage.vue'
+import CertificatesPage from '@/pages/CertificatesPage.vue'
 
 const routes = [
   //{path: "/", name: 'DashboardPage', component: DashboardPage} //KEVIN'S DASHBOARD
   {path: "/", name: 'MyAccountPage', component: MyAccountPage}, // CRISTOBE
   // L'S MY_ACCOUNT
-  { path: '/courses', name: 'CoursesPage', component: CoursesPage,}
+  { path: '/courses', name: 'CoursesPage', component: CoursesPage,}, 
+ { path: '/Certificates', name:'CertificatesPage', component: CertificatesPage,} //UDAY KYAMA
 ]
 
 const router = createRouter({
