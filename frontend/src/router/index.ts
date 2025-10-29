149fce57fad8673c7faf46be57ef625bc8f7f8e3
vincentlam4554 <vlam3@csus.edu>
2025-10-29 15:20:24 -0700

    Restore router file and finalize merge

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,36 +1,24 @@
-<<<<<<< HEAD
-import { createRouter, createWebHistory } from 'vue-router'
+ import { createRouter, createWebHistory } from 'vue-router'
 import DashboardPage from '../pages/DashboardPage.vue'
 import MyAccountPage from '../pages/MyAccountPage.vue'
 import CoursesPage from '../pages/CoursesPage.vue'
 import OPnum from '../pages/OperatorNumbers.vue'
 import CertificatesPage from '@/pages/CertificatesPage.vue'
+import PurchaseHistory from '../pages/PurchaseHistory.vue'
 
 const routes = [
-  //{path: "/", name: 'DashboardPage', component: DashboardPage} //KEVIN'S DASHBOARD
-  {path: "/", name: 'Learning Portal', component: DashboardPage}, // CRISTOBE
+  // { path: "/", name: 'DashboardPage', component: DashboardPage } // KEVIN'S DASHBOARD
+  { path: "/", name: 'Learning Portal', component: DashboardPage }, // CRISTOBE
   // L'S MY_ACCOUNT
-  { path: '/courses', name: 'CoursesPage', component: CoursesPage,},
-  { path: '/operatornumbers', name: 'Operator Numbers', component:OPnum,},
-  { path: '/Certificates', name:'CertificatesPage', component: CertificatesPage,} //UDAY KYAMA
+  { path: '/courses', name: 'CoursesPage', component: CoursesPage },
+  { path: '/operatornumbers', name: 'Operator Numbers', component: OPnum },
+  { path: '/Certificates', name: 'CertificatesPage', component: CertificatesPage }, // UDAY KYAMA
+  { path: '/purchase-history', name: 'PurchaseHistory', component: PurchaseHistory } // VINCENT LAM
 ]
-=======
-//tets commit
-import { createRouter, createWebHistory } from 'vue-router';
-import PurchaseHistory from '../pages/PurchaseHistory.vue';
-
-const routes = [
-  {
-    path: '/purchase-history',
-    name: 'purchase-history',
-    component: PurchaseHistory,
-  },
-];
->>>>>>> vincent-purchasehistory-page
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes,
-});
+  routes
+})
 
-export default router;
+export default router
