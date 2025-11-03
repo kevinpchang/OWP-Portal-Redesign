c308b1f3c3ac78319e9bf3e82daee872b02ffa24
ry_cran <ryancranda@gmail.com>
2025-11-02 16:32:50 -0800

    Update Operator Numbers popups
    Added Add number and edit/remove number popups + buttons to open popups + formatting.

--- frontend/src/pages/OperatorNumbers.vue
+++ frontend/src/pages/OperatorNumbers.vue
@@ -5,12 +5,12 @@
     </div>
 
     <div name="button">
-      <button class = add-button type="button">Add Operatorn Number</button>
+      <button class = add-button popovertarget="addpopover">Add Operator Number</button>
     </div>
 
     <div name="table">
       <table>
-        <thead class="tile-header">
+        <thead class="table-header">
             <tr>
                 <th>State/Province</th>
                 <th>Operator Number</th>
@@ -21,23 +21,49 @@
             <tr>
                 <td>California</td>
                 <td>12345</td>
-                <td>Edit/Remove</td>
+                <td><button class="edit-button" popovertarget="editpopover">Edit/Remove</button></td>
             </tr>
             <tr>
                 <td>Colorado</td>
                 <td>67890</td>
-                <td>Edit/Remove</td>
+                <td><button class="edit-button" popovertarget="editpopover">Edit/Remove</button></td>
             </tr>
             <tr>
                 <td>Washington</td>
                 <td>59746</td>
-                <td>Edit/Remove</td>
+                <td><button class="edit-button" popovertarget="editpopover">Edit/Remove</button></td>
             </tr>
         </tbody>
       </table>
     </div>
+
+    <div id="addpopover" popover="manual" class="popup">
+      <form class="form-container">
+        <h1 class="popup-title">
+          Add Operator Number
+        </h1>
+        <input type="text" id="state" class="input-box" placeholder="State/Province"><br><br>
+        <input type="text" id="opnum" class="input-box" placeholder="Operator Number"><br><br>
+        <button class = popup-button-left popovertarget="addpopover" popovertargetaction="hide">Add</button>
+
+      </form>
+    </div>
+
+    <div id="editpopover" popover="manual" class="popup">
+      <form class="form-container">
+        <h1 class="popup-title">
+          Edit/Remove Operator Number
+        </h1>
+        <input type="text" id="state" class="input-box" placeholder="State/Province"><br><br>
+        <input type="text" id="opnum" class="input-box" placeholder="Operator Number"><br><br>
+        <button class = popup-button-left popovertarget="addpopover" popovertargetaction="hide">Edit</button>
+        <button class = popup-button-right popovertarget="editpopover" popovertargetaction="hide">Delete</button>
+
+      </form>
+    </div>
   </div>
 </template>
+
 
 <style scoped>
 .operator-numbers-page {
@@ -65,14 +91,8 @@
   align-items: center;
 }
 
-.page-description {
-  font-size: 16px;
-  color: #555;
-  font-family: 'Myriad Pro', sans-serif;
-  margin: 0;
-}
-
 .add-button {
+  position: relative;
   margin-left: 55%;
   background-color: #48773C;
   padding:15px 15px;
@@ -82,26 +102,38 @@
   color:white
 }
 
-.tiles-container {
-    margin-top: 46px;
-    padding-left: 18px;
-    display: flex;
-    flex-direction: column;
-    gap: 16px;
+.edit-button{
+  background: transparent;
+  border: none !important;
+  font-family: 'Myriad Pro', sans-serif;
+  font-size: 17px;
+  font-weight: 600;
+  color: #034750;
 }
 
-.table-tile {
-    background-color: #f2f1f2;
-    border-radius: 14px;
-    padding: 24px;
-    width: 700px;
-    min-height: 150px;
-    display: flex;
-    flex-direction: column;
-    gap: 12px;
+.popup-button-left{
+  margin-left: 15px;
+  margin-top:15px;
+  background-color: #48773C;
+  padding:15px 15px;
+  border:none;
+  border-radius: 10px;
+  font-family: 'Myriad Pro, Bold', sans-serif;  
+  color:white
 }
 
-.tile-header {
+.popup-button-right{
+  margin-left: 45%;
+  margin-top:15px;
+  background-color: #48773C;
+  padding:15px 15px;
+  border:none;
+  border-radius: 10px;
+  font-family: 'Myriad Pro, Bold', sans-serif;  
+  color:white
+}
+
+.table-header {
     font-family: 'Myriad Pro', sans-serif;
     font-size: 20px;
     font-weight: 600;
@@ -159,8 +191,50 @@ table {
     border-bottom: 1px solid #ddd;
   }
 
-  .content-body {
-    margin-top: 32px;
-    padding: 0 32px;
-}
+  .popup{
+    margin-top: 15%;
+    width: 20%;
+    height: 30%;
+    position: fixed;
+    border: 3px solid #004d4d;
+    border-radius: 10px;
+    border-top: 75px solid #004d4d;
+    background-color: #f2f1f2;
+  }
+
+  .popup-title{
+    margin-left: 20px;
+    margin-top: 20px;
+    font-family: 'Myriad Pro, Bold', sans-serif;
+    font-size: 28px;
+    font-weight: 790;
+    color: #034750;
+    align-items: center;
+  }
+
+  .input-box {
+    width: 80%;
+    height: 60px;
+    margin-left: 15px;
+    margin-top: 15px;
+    background-color: #e8e8e8; /* light gray background */
+    border: none;
+    border-radius: 8px; /* rounded corners */
+    font-size: 18px;
+    padding: 0 20px;
+    box-sizing: border-box;
+    color: #555;
+  }
+
+  .input-box::placeholder {
+    color: #ccc; /* light gray placeholder text */
+    font-size: 18px;
+  }
+
+  .input-box:focus {
+    outline: none; /* removes blue outline */
+    background-color: #e1e1e1;
+  }
 </style>
+
+
